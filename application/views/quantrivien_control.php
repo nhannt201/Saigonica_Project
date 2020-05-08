f8f74c88e5615ef29740b0ce5cba8fb233264f27
nhannt201 <trungnhan21.12@gmail.com>
2020-05-08 18:55:30 +0700

    Add Admin files. DEMO.

--- application/views/quantrivien_control.php
+++ application/views/quantrivien_control.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Saigonica - Nền tảng Blog nhanh!</title>
+	<title>Admin - Saigonica</title>
 
 	<style type="text/css">
 
@@ -71,15 +71,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<h1>Chào mừng bạn đến với Saigonica!</h1>
 
 	<div id="body">
-		<p>Trang cho <a href="/index.php/QuanTriVien">Quản Trị Viên.</a></p>
-<hr/>
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+		<p>Đây là trang Quản Trị Viên.</p>
 
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+	
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
