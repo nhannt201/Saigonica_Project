035dc5f8b2daf4b9b21147f3fbf868cefd6f594f
bkk711 <boylaboy8@gmail.com>
2020-05-09 09:19:34 +0700

    Add model Bai Viet moi

---
+++ application/models/Baivietmoi.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Baivietmoi extends CI_Model {
+
+	public function loadbai($sobai)
+	{
+		mysql_query(query);
+	}
+
+}
+
+/* End of file Baivietmoi.php */
+/* Location: ./application/models/Baivietmoi.php */
