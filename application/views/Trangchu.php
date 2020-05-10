8c56b391952d61f386961a16b6f6b7c3319c4cb3
nhannt201 <trungnhan21.12@gmail.com>
2020-05-10 09:47:00 +0700

    Update style.

--- application/views/Trangchu.php
+++ application/views/Trangchu.php
@@ -6,12 +6,117 @@
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 </head>
 <body>
-<article class="container">
+<nav class="navbar navbar-default" role="navigation">
+
+  <div class="container-fluid">
+
+    <!-- Brand and toggle get grouped for better mobile display -->
+
+    <div class="navbar-header">
+
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+
+        <span class="sr-only">Toggle navigation</span>
+
+        <span class="icon-bar"></span>
+
+        <span class="icon-bar"></span>
+
+        <span class="icon-bar"></span>
+
+      </button>
+
+      <a class="navbar-brand" href="#">Saigonica</a>
+
+    </div>
+
+
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+      <ul class="nav navbar-nav">
+
+        <li class="active"><a href="/">Trang chủ</a></li>
+
+        <li><a href="/Admin">Quản trị</a></li>
+
+      <!--  <li class="dropdown">
+
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+
+          <ul class="dropdown-menu">
+
+            <li><a href="#">Action</a></li>
+
+            <li><a href="#">Another action</a></li>
+
+            <li><a href="#">Something else here</a></li>
+
+            <li class="divider"></li>
+
+            <li><a href="#">Separated link</a></li>
+
+            <li class="divider"></li>
+
+            <li><a href="#">One more separated link</a></li>
+
+          </ul>
+
+        </li>-->
+
+      </ul>
+
+      <form class="navbar-form navbar-left" role="search">
+
+        <div class="form-group">
+
+          <input type="text" class="form-control" placeholder="cảm giác buồn">
+
+        </div>
+
+        <button type="submit" class="btn btn-default">Tìm kiếm</button>
+
+      </form>
+
+      <ul class="nav navbar-nav navbar-right">
+
+        <li><a href="#">Xin chao : Lee Nann Anh</a></li>
+
+        <li class="dropdown">
+
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Cài đặt  <span class="caret"></span></a>
+
+          <ul class="dropdown-menu">
+
+            <li><a href="#">Action</a></li>
+
+            <li><a href="#">Another action</a></li>
+
+            <li><a href="#">Something else here</a></li>
+
+            <li class="divider"></li>
+
+            <li><a href="#">Separated link</a></li>
+
+          </ul>
+
+        </li>
+
+      </ul>
+
+    </div><!-- /.navbar-collapse -->
+
+  </div><!-- /.container-fluid -->
+
+</nav>
+<!--
 <header class="jumbotron text-center">
 <h2>Saigonica - Nền tảng Blog nhanh!</h2>
 <p class="text-warning">Chia sẻ mọi cảm xúc của bạn!</p>
-</header>
-
+</header> -->
+<article class="container">
 <div class="row">
 <div class="col-md-9">
 <!--Nam vao vung can le trai-->
@@ -76,12 +181,16 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia doloremque min
 <div class="col-md-3">
 <div class="panel panel-default">
 <div class="list-group-item active">
-Xin chao : <b>Lee Nann Anh</b>
+Introduction
 </div>
 <div class="list-group-item">
 <h4 class="list-group-item-heading"><center>Micro BLog</center></h4>
 <p class="list-group-item-text"> Chia se nhung bai viet cua ban
 . viet nen cam xuc cau ban</p>
+</div>
+<div class="list-group-item">
+<h4 class="list-group-item-heading"><center>About me</center></h4>
+<p class="list-group-item-text">My name is XXX, I am a doctor and like write blogs</p>
 </div>
 <div class="list-group-item">
 <h4 class="list-group-item-heading"><center>Thong KE</center></h4>
