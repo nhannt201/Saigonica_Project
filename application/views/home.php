9eb31d72838ccd836112251501e235a341f4545f
nhannt201 <trungnhan21.12@gmail.com>
2020-05-13 09:39:39 +0700

    thêm lang ="vi"

--- application/views/home.php
+++ application/views/home.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><html>
+?><html lang="vi">
 <head>
 <title>Saigonica - Nền tảng Blog nhanh!</title>
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
--- application/views/profile.php
+++ application/views/profile.php
@@ -1,83 +1,256 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
+?><html lang="vi">
 <head>
-	<meta charset="utf-8">
-	<title>Profile - Saigonica</title>
-
-	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+<title>Saigonica - Nền tảng Blog nhanh!</title>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 </head>
 <body>
+<nav class="navbar navbar-default" role="navigation">
 
-<div id="container">
-	<h1>Chào mừng bạn đến với Saigonica!</h1>
+  <div class="container-fluid">
 
-	<div id="body">
-		<p>Đây là trang cá nhân.</p>
+    <!-- Brand and toggle get grouped for better mobile display -->
 
-	
-	</div>
+    <div class="navbar-header">
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+
+        <span class="sr-only">Toggle navigation</span>
+
+        <span class="icon-bar"></span>
+
+        <span class="icon-bar"></span>
+
+        <span class="icon-bar"></span>
+
+      </button>
+
+      <a class="navbar-brand" href="#">Saigonica</a>
+
+    </div>
+
+
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+      <ul class="nav navbar-nav">
+
+        <li class="active"><a href="/">Trang chủ</a></li>
+
+        <li><a href="/Admin">Quản trị</a></li>
+
+      <!--  <li class="dropdown">
+
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+
+          <ul class="dropdown-menu">
+
+            <li><a href="#">Action</a></li>
+
+            <li><a href="#">Another action</a></li>
+
+            <li><a href="#">Something else here</a></li>
+
+            <li class="divider"></li>
+
+            <li><a href="#">Separated link</a></li>
+
+            <li class="divider"></li>
+
+            <li><a href="#">One more separated link</a></li>
+
+          </ul>
+
+        </li>-->
+
+      </ul>
+
+      <form class="navbar-form navbar-left" role="search">
+
+        <div class="form-group">
+
+          <input type="text" class="form-control" placeholder="cảm giác buồn">
+
+        </div>
+
+        <button type="submit" class="btn btn-default">Tìm kiếm</button>
+
+      </form>
+
+      <ul class="nav navbar-nav navbar-right">
+
+        <li><a href="/profile">Xin chao : Lee Nann Anh</a></li>
+
+        <li class="dropdown">
+
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Cài đặt  <span class="caret"></span></a>
+
+          <ul class="dropdown-menu">
+
+            <li><a href="#">Action</a></li>
+
+            <li><a href="#">Another action</a></li>
+
+            <li><a href="#">Something else here</a></li>
+
+            <li class="divider"></li>
+
+            <li><a href="#">Separated link</a></li>
+
+          </ul>
+
+        </li>
+
+      </ul>
+
+    </div><!-- /.navbar-collapse -->
+
+  </div><!-- /.container-fluid -->
+
+</nav>
+<!--
+<header class="jumbotron text-center">
+<h2>Saigonica - Nền tảng Blog nhanh!</h2>
+<p class="text-warning">Chia sẻ mọi cảm xúc của bạn!</p>
+</header> -->
+<article class="container">
+<div class="row">
+<div class="col-md-9">
+<!--Nam vao vung can le trai-->
+<div class="panel panel-default">
+<div class="panel-body">
+
+<form><!--Form post trang thai-->
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Bạn đang cảm thấy gì?</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Tôi cảm thấy..." rows="3"></textarea>
+	  </div>
+	   <div class="text-right">
+	 <button type="submit" class="btn btn-default">Cập nhật</button>
+	   </div>
+	    </div>
+		 </div>
+</form>
+<div class="panel panel-default">
+<div class="list-group-item">
+<p><b><a>Hoa Thiên Cốt</a></b> <span class="pull-right">10. 5. 2020</span></p>
+Hôm nay trời nóng quá!!! Tôi mới làm ly đá bào ăn cho bỏ nóng này!
+<br /><button class="btn btn-xs btn-default"><b>Thích</b> 500</button><button class="btn btn-xs btn-default"><b>Tim</b> 1000</button>
 </div>
-
+<div class="list-group-item">
+<p><b><a>Lee Nann Anh</a></b> <span class="pull-right">1. 5. 2020</span></p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia doloremque minima ab? Id provident aperiam atque excepturi repudiandae quis eveniet expedita dolores esse doloremque quasi sed magnam commodi sit modi!
+<br /><button class="btn btn-xs btn-default"><b>Like</b> 500</button>
+</div>
+<div class="list-group-item">
+<p><b><a>Lee Nann Anh</a></b> <span class="pull-right">1. 5. 2020</span></p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia doloremque minima ab? Id provident aperiam atque excepturi repudiandae quis eveniet expedita dolores esse doloremque quasi sed magnam commodi sit modi!
+<br /><button class="btn btn-xs btn-default"><b>Like</b> 500</button>
+</div>
+<div class="list-group-item">
+<p><b><a>Lee Nann Anh</a></b> <span class="pull-right">1. 5. 2020</span></p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia doloremque minima ab? Id provident aperiam atque excepturi repudiandae quis eveniet expedita dolores esse doloremque quasi sed magnam commodi sit modi!
+<br /><button class="btn btn-xs btn-default"><b>Like</b> 500</button>
+</div>
+<div class="list-group-item">
+<p><b><a>Lee Nann Anh</a></b> <span class="pull-right">1. 5. 2020</span></p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia doloremque minima ab? Id provident aperiam atque excepturi repudiandae quis eveniet expedita dolores esse doloremque quasi sed magnam commodi sit modi!
+<br /><button class="btn btn-xs btn-default"><b>Like</b> 500</button>
+</div>
+<div class="list-group-item">
+<p><b><a>Lee Nann Anh</a></b> <span class="pull-right">1. 5. 2020</span></p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia doloremque minima ab? Id provident aperiam atque excepturi repudiandae quis eveniet expedita dolores esse doloremque quasi sed magnam commodi sit modi!
+<br /><button class="btn btn-xs btn-default"><b>Like</b> 500</button>
+</div>
+<div class="list-group-item">
+<p><b><a>Lee Nann Anh</a></b> <span class="pull-right">1. 5. 2020</span></p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia doloremque minima ab? Id provident aperiam atque excepturi repudiandae quis eveniet expedita dolores esse doloremque quasi sed magnam commodi sit modi!
+<br /><button class="btn btn-xs btn-default"><b>Like</b> 500</button>
+</div>
+<div class="list-group-item">
+<ul class="pager">
+<li class="previous"><a href="#">&larr; Older</a></li>
+<li class="next"><a href="#">Newer &rarr;</a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="col-md-3">
+<div class="panel panel-default">
+<div class="list-group-item active">
+Introduction
+</div>
+<div class="list-group-item">
+<h4 class="list-group-item-heading"><center>Micro BLog</center></h4>
+<p class="list-group-item-text"> Chia se nhung bai viet cua ban
+. viet nen cam xuc cau ban</p>
+</div>
+<div class="list-group-item">
+<h4 class="list-group-item-heading"><center>About me</center></h4>
+<p class="list-group-item-text">My name is XXX, I am a doctor and like write blogs</p>
+</div>
+<div class="list-group-item">
+<h4 class="list-group-item-heading"><center>Thong KE</center></h4>
+<p>post <span class="pull-right">10</span></p>
+<p>usser <span class="pull-right">10</span></p>
+<p>like <span class="pull-right">10</span></p>
+<p>commet <span class="pull-right">10</span></p>
+<p>view <span class="pull-right">10</span></p>
+<p>traffic <span class="pull-right">10</span></p>
+</div>
+<div class="list-group-item">
+<h4 class="list-group-item-heading"><center>Thanh Vien</center></h4><br />
+<div class="row">
+<div class="col-xs-6 col-md-3">
+<a href="#" class="thumbnail">
+<img data-src="holder.js/100%x180" alt="100%x180" style=" width: 100%; display: block;" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMzYiIGhlaWdodD0iMTgwIj48cmVjdCB3aWR0aD0iMTM2IiBoZWlnaHQ9IjE4MCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjY4IiB5PSI5MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjEzNngxODA8L3RleHQ+PC9zdmc+">
+</a>
+</div>
+<div class="col-xs-6 col-md-3">
+<a href="#" class="thumbnail">
+<img data-src="holder.js/100%x180" alt="100%x180" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMzYiIGhlaWdodD0iMTgwIj48cmVjdCB3aWR0aD0iMTM2IiBoZWlnaHQ9IjE4MCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjY4IiB5PSI5MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjEzNngxODA8L3RleHQ+PC9zdmc+" style=" width: 100%; display: block;">
+</a>
+</div>
+<div class="col-xs-6 col-md-3">
+<a href="#" class="thumbnail">
+<img data-src="holder.js/100%x180" alt="100%x180" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMzYiIGhlaWdodD0iMTgwIj48cmVjdCB3aWR0aD0iMTM2IiBoZWlnaHQ9IjE4MCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjY4IiB5PSI5MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjEzNngxODA8L3RleHQ+PC9zdmc+" style=" width: 100%; display: block;">
+</a>
+</div>
+<div class="col-xs-6 col-md-3">
+<a href="#" class="thumbnail">
+<img data-src="holder.js/100%x180" alt="100%x180" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMzYiIGhlaWdodD0iMTgwIj48cmVjdCB3aWR0aD0iMTM2IiBoZWlnaHQ9IjE4MCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjY4IiB5PSI5MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjEzNngxODA8L3RleHQ+PC9zdmc+" style=" width: 100%; display: block;">
+</a>
+</div>
+<div class="col-xs-6 col-md-3">
+<a href="#" class="thumbnail">
+<img data-src="holder.js/100%x180" alt="100%x180" style=" width: 100%; display: block;" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMzYiIGhlaWdodD0iMTgwIj48cmVjdCB3aWR0aD0iMTM2IiBoZWlnaHQ9IjE4MCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjY4IiB5PSI5MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjEzNngxODA8L3RleHQ+PC9zdmc+">
+</a>
+</div>
+<div class="col-xs-6 col-md-3">
+<a href="#" class="thumbnail">
+<img data-src="holder.js/100%x180" alt="100%x180" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMzYiIGhlaWdodD0iMTgwIj48cmVjdCB3aWR0aD0iMTM2IiBoZWlnaHQ9IjE4MCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjY4IiB5PSI5MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjEzNngxODA8L3RleHQ+PC9zdmc+" style=" width: 100%; display: block;">
+</a>
+</div>
+<div class="col-xs-6 col-md-3">
+<a href="#" class="thumbnail">
+<img data-src="holder.js/100%x180" alt="100%x180" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMzYiIGhlaWdodD0iMTgwIj48cmVjdCB3aWR0aD0iMTM2IiBoZWlnaHQ9IjE4MCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjY4IiB5PSI5MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjEzNngxODA8L3RleHQ+PC9zdmc+" style=" width: 100%; display: block;">
+</a>
+</div>
+<div class="col-xs-6 col-md-3">
+<a href="#" class="thumbnail">
+<img data-src="holder.js/100%x180" alt="100%x180" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMzYiIGhlaWdodD0iMTgwIj48cmVjdCB3aWR0aD0iMTM2IiBoZWlnaHQ9IjE4MCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjY4IiB5PSI5MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjEzNngxODA8L3RleHQ+PC9zdmc+" style=" width: 100%; display: block;">
+</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</article>
 </body>
 </html>
