3534b9f7f23db9b9723a6eeca8490cd1aec2c4e3
bkk711 <boylaboy8@gmail.com>
2020-05-13 21:48:57 +0700

    edit 1 so thu

--- application/controllers/Trangchu.php
+++ application/controllers/Trangchu.php
@@ -2,12 +2,23 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Trangchu extends CI_Controller {
+	private  $checkform = FALSE;
 
 	public function index()
 	{
 		$this->load->view('Trangchu');		
 	}
-
+	public function form_post()
+	{
+		$this->form_validation->set_rules('posts', 'posts');
+		if($this->form_validation->run() == TRUE) {
+			$this->checkform = TRUE;
+		}
+		$data['checkform'] = $this->checkform;
+		$this->load->view('Trangchu', $data);
+			
+		
+	}
 }
 
 /* End of file Trangchu.php */
