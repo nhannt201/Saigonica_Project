51ee4d69d979639b36ef1f3b87568d047d63e8bc
nhannt201 <trungnhan21.12@gmail.com>
2020-05-13 10:58:31 +0700

    Improve Profile Style

--- application/views/profile.php
+++ application/views/profile.php
@@ -9,29 +9,103 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </head>
 <body>
 <?php include("_menu.php") ?>
-<!--
-<header class="jumbotron text-center">
-<h2>Saigonica - Nền tảng Blog nhanh!</h2>
-<p class="text-warning">Chia sẻ mọi cảm xúc của bạn!</p>
-</header> -->
+
 <article class="container">
 <div class="row">
 <div class="col-md-9">
 <!--Nam vao vung can le trai-->
+<!--Profile-->
+ <style>
+ #avt{
+	 width: 100px;
+ }
+ </style>
 <div class="panel panel-default">
-<div class="panel-body">
 
-<form><!--Form post trang thai-->
-  <div class="form-group">
-    <label for="exampleFormControlTextarea1">Bạn đang cảm thấy gì?</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Tôi cảm thấy..." rows="3"></textarea>
-	  </div>
-	   <div class="text-right">
-	 <button type="submit" class="btn btn-default">Cập nhật</button>
+  <div class="panel-heading">
+
+    <h3 class="panel-title">Thông tin cá nhân</h3>
+
+  </div>
+
+  <div class="panel-body">
+  <div class="row">
+	<div class="col-md-3">
+		<!--ShowAVT-->
+		<div class="text-center">
+          <img src="https://i.imgur.com/ntrJCDc.jpg" id="avt" alt="...">	  
+		   <ul class="pager">	
+				<li><a>Avatar hiện tại</a></li>
+			</ul>
+		  </div>
+		  </div>
+		  <div class="col-md-9">
+		<!--ChooseAVT-->
+<div class="input-group">
+  <span class="input-group-addon">Chọn ảnh đại diện</span>
+  <input type="file" class="form-control">
+</div>
+	<!--endchose-->
+	<br/>
+	<!--InforPrivate-->
+<div class="input-group">
+  <span class="input-group-addon">Họ và tên:</span>
+  <input type="text" class="form-control" value="Sơn Tùng MTP" placeholder="Trần Văn B">
+</div>
+<!--Save-->
+<br/>
+	    <div class="text-right">
+	 <button type="submit" class="btn btn-default">Lưu thông tin</button>
 	   </div>
-	    </div>
-		 </div>
+		</div>
+	</div>
+  </div>
+
+</div>
+<!--ThongtinPage-->
+<div class="panel panel-default">
+
+  <div class="panel-heading">Thông tin trang</div>
+
+  <div class="panel-body">
+
+  <form>
+
+
+<div class="panel-body">
+  <!--Input name blog-->
+  <!--Form Intro Page-->
+  <div class="input-group">
+  <span class="input-group-addon">Tên Blog</span>
+  <input type="text" class="form-control" value="Những nồi niềm tâm tư..." placeholder="Những nồi niềm tâm tư...">
+  </div><br/>
+      <!--Input intro blog-->
+ <label>Giới thiệu blog:</label>
+   <div class="form-group">
+    <textarea class="form-control" rows="3">Xin chào các bạn, blog tôi chia sẻ tin tức hót hòn họt đây nà!</textarea>
+  </div>
+  
+
+	<!--Radio-->
+<div class="form-group">
+  <label for="sel1">Hiển thị:</label>
+  <select class="form-control" id="sel1">
+    <option>Công khai</option>
+    <option>Riêng tư</option>
+  </select>
+</div>
+  <!--End Radio-->
+    <div class="text-right">
+	 <button type="submit" class="btn btn-default">Lưu thông tin</button>
+	   </div>
 </form>
+
+  </div>
+
+</div>
+</div>
+
+<!--Vung can le-->
 </article>
 </body>
 </html>
